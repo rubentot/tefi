82402dbc919acd24af1bec98b8fb6c7bda16bfd8
rubentotterman <ruben_totterman@hotmail.com>
2025-07-18 15:03:07 +0200

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="no">
       <body className={inter.className}>
-        <Providers>  // Wrap here
+        <Providers>
           {children}
         </Providers>
       </body>
